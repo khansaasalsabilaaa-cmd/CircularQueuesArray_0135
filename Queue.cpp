508fb6e79855ab3fc15f2f66f1e7fa9ff86bb5e2
khansaasasabilaaa-eng <khansaasasabilaaa@gmail.com>
2026-05-28 10:41:06 +0700

    menambahkan cek jika antrian hanya satu elemen

--- Queue.cpp
+++ Queue.cpp
@@ -55,7 +55,16 @@ public:
             }
             cout << "\nhe element deleted from the queue is: " << queue_array[FRONT] << "\n";
 
-            
+            // cek jika antrian hanya memiliki satu elemen
+            if (FRONT == REAR)
+            {
+                FRONT = -1;
+                REAR = -1;
+            }
+            else
+            {
+                
+            }
         }
     }
 }
